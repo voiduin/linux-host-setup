e36b4b9b652ef370efa5c813006a6043e1c67546
Vitya <62399526+voiduin@users.noreply.github.com>
2024-04-08 22:40:49 +0300

    [configure_ssh.bash] Replace create_backup_for_file()
    Replace this function from specific sshd_configure.bash file

--- configure_ssh.bash
+++ configure_ssh.bash
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Set bash execution flags:
+# - Treat unset variables as an error when substituting
+# - Exit immediately if a command exits with a non-zero status
+# - Print each command to stdout before executing it (useful for debugging)
+set -u
+# set -e
+# set -x
+
 # Base URL of the repository
 REPO_URL="https://raw.githubusercontent.com/voiduin/linux-host-setup/main"
 
@@ -13,7 +21,30 @@ run_rscript () {
     curl -Ls "$REPO_URL/${script}.bash" | sudo bash -s -- "${params[@]}"
 }
 
-# Example of running a script with parameters
-run_rscript sshd_configure Port 2221
+# Usage example: create_backup_for_file "/etc/ssh/sshd_config"
+create_backup_for_file() {
+    local file_path="$1"
+    local backup_dir="${2:-$(dirname "$file_path")}"
+    local old_filename="$(basename -- "$file_path")"
+    local backup_path="${backup_dir}/${old_filename}.$(date +%y%m%d_%H%M%Sms).bac"
+    cp "$file_path" "$backup_path" || exit_with_err "Failed to create backup file: $backup_path."
+    echo "Backup created: $backup_path"
+}
 
-echo "Config ended"
+main() {
+    local config_file_path="/etc/ssh/sshd_config"
+
+    # Create new user
+    # TODO:this
+
+    # Configure SSH config file
+    create_backup_for_file "${config_file_path}"
+    run_rscript sshd_configure Port 2221
+   
+    # Install fail2ban
+    # TODO:this
+
+    echo "Config ended"
+}
+
+main "$@"
